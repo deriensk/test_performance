b4adabc21503d99b9c3a38434e0d2849fea9c239
Sudhamsu Khanal <sudhamsukhanal@gmail.com>
2016-12-05 21:45:33 +0545

    querying the db list

--- src/performance/urls.py
+++ src/performance/urls.py
@@ -1,9 +1,10 @@
 from django.conf.urls import url, include
 
-from .views import student_info, student_subject
+from .views import student_info, student_subject, student_list
 
 urlpatterns = [
    
+   url(r'^$', student_list, name='home'),
    url(r'^info/$', student_info, name='student'),
    url(r'^info/(?P<id>\d+)/$', student_subject, name='subject'),
 
--- src/performance/views.py
+++ src/performance/views.py
@@ -1,8 +1,10 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, redirect
 from .models import Student, Subject
 from .forms import StudentModelForm, SubjectModelForm
 from django.http import HttpResponseRedirect
 # Create your views here.
+
+
 
 def student_info(request):
 	student_form = StudentModelForm(request.POST or None)
@@ -21,9 +23,17 @@ def student_subject(request, id=None):
 	if subject_form.is_valid():
 		instance = subject_form.save(commit=False)
 		instance.save()
-		return HttpResponseRedirect()
+		return redirect('home')
 	context = {
 		'subject':subject_form,
 	}	
 
 	return render(request, 'subject.html', context)
+
+def student_list(request):
+	queryset = Subject.objects.all().order_by('-id')
+	context = {
+		'title':'List on its way',
+		'queryset':queryset
+	}
+	return render(request, 'home.html', context)
